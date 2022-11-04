7de6ff62c2f68b81ef23e0fd3129d6b8e73bc456
Ashlesha Mainali <axm08111@vsc.edu>
2022-11-03 23:51:13 -0400

    Part 2

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,7 +1,11 @@
 let words = [];
-
+// search words for personal dictonary 
 function Search (words){
+    return words.includes(word);
 }
 function addWord (words){
+    words.push(word);
 }
 export {Search, addword}
+
+
